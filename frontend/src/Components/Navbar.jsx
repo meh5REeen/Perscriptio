14f7e84f344928e3fc66ef2bd832b7e319a2c82f
meh5REeen <mehreens826@gmail.com>
2025-07-24 00:47:31 +0500

    completed Nav

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {assets} from '../assets/assets_frontend/assets.js';
 import {NavLink, useNavigate} from 'react-router-dom';
 const Navbar = () => {
   const navigate = useNavigate();
+  const [showMenu,setShowMenu] = useState(false);
+  const [token,setToken] = useState(true);
+
   return (
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
       <img className='w-44 cursor-pointer' src={assets.logo} alt="logo"/>
@@ -26,8 +29,32 @@ const Navbar = () => {
         </NavLink>
       </ul>
       <div className='flex items-center gap-4'>
+        {
+          token ? 
+          <div className='flex items-center gap-2 cursor-pointer group relative'>
+            <img
+                className='w-8 rounded-full' 
+                src = {assets.profile_pic}
+                alt="profile picture"
+            />
+            <img  
+                className='w-2.5'
+                src={assets.dropdown_icon}/>
+          
+            <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
+                  <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4 '>
+                      <p onClick={()=>{navigate('/profile')}} className='hover:text-black cursor-pointer'>My Profile</p>
+                      <p onClick={()=>{navigate('/my-appointments')}} className='hover:text-black cursor-pointer'>My Appointments</p>
+                      <p onClick={()=>{setToken(false)}} className='hover:text-black cursor-pointer'>Logout</p>
+
+                  </div>
+            
+            </div>
+          </div>
+          : 
+        
         <button onClick={()=>navigate('/login')}
-         className='bg-[#5f6FFF] text-white px-8 py-3 rounded-full font-light hidden md:block'>Create Account</button>
+         className='bg-[#5f6FFF] text-white px-8 py-3 rounded-full font-light hidden md:block'>Create Account</button>}
       </div>
     </div>
   )
