06839b23ff1b7e372627f5980902683fb7acf383
meh5REeen <mehreens826@gmail.com>
2025-07-26 13:28:56 +0500

    banner component and context added

--- frontend/src/Components/TopDoctors.jsx
+++ frontend/src/Components/TopDoctors.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
-import { doctors } from '../assets/assets_frontend/assets'
+import React, { useContext } from 'react'
+import {useNavigate} from "react-router-dom"
+import { AppContext } from '../context/context';
+
 const TopDoctors = () => {
+  const navigate = useNavigate();
+  const {doctors} = useContext(AppContext);
   return (
     <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10 '>
       <h1 className='text-3xl font-medium'>Top Doctors to Book</h1>
@@ -8,7 +12,9 @@ const TopDoctors = () => {
 
 <div className='w-full grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-4 pt-5 gap-y-6 px-3 sm:px-0'>            {
                 doctors.slice(0,10).map((item,index)=>(
-                    <div key={index} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10x] transition-all duration-500'>
+                    <div
+                          onClick={()=>navigate(`/appointment/${item._id}`)}
+                         key={index} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10x] transition-all duration-500'>
                         <img className='bg-blue-50 ' 
                         src={item.image} alt=""/>
 
@@ -28,7 +34,9 @@ const TopDoctors = () => {
                 ))
             }
         </div>
-        <button className='bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10'>More</button>
+        <button 
+        onClick={()=> {navigate('/doctors');scrollTo(0,0)}}
+        className='bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10'>More</button>
     </div>
   )
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../Components/Header'
 import SpecialityMenu from '../Components/SpecialityMenu'
 import TopDoctors from '../Components/TopDoctors'
+import Banner from '../Components/Banner'
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Header/>
       <SpecialityMenu/>
       <TopDoctors/>
+      <Banner/>
       home
     </div>
   )
