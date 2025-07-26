ff31ecddb9dd7d05ab7483c799d15b8e5902af97
meh5REeen <mehreens826@gmail.com>
2025-07-26 12:48:44 +0500

    Top doctors component added

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../Components/Header'
 import SpecialityMenu from '../Components/SpecialityMenu'
+import TopDoctors from '../Components/TopDoctors'
 
 export default function Home() {
   return (
     <div>
       <Header/>
       <SpecialityMenu/>
+      <TopDoctors/>
       home
     </div>
   )
