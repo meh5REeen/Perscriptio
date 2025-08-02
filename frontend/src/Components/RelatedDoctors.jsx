7cd5bf558d8cf8a8bb178e07603b786e64218670
meh5REeen <mehreens826@gmail.com>
2025-08-03 03:08:18 +0500

    Appointment page done

--- frontend/src/Components/RelatedDoctors.jsx
+++ frontend/src/Components/RelatedDoctors.jsx
@@ -1,19 +1,29 @@
-import React, { useContext } from 'react'
-import {useNavigate} from "react-router-dom"
-import { AppContext } from '../context/AppContext';
+import React, { useContext, useEffect, useState } from 'react'
+import { AppContext } from '../context/AppContext'
+import { useNavigate } from 'react-router-dom';
 
-const TopDoctors = () => {
-  const navigate = useNavigate();
-  const {doctors} = useContext(AppContext);
+const RelatedDoctors = ({docId,speciality}) => {
+    const {doctors} = useContext(AppContext);
+    const [relDoc,setrelDoc] = useState([]);
+    const navigate = useNavigate();
+
+    useEffect(()=>{
+        if(doctors.length > 0 && speciality){
+            const doctorsData = doctors.filter((doc)=> doc.speciality === speciality && doc._id !== docId);
+            setrelDoc(doctorsData);
+        }
+    },[doctors,speciality,docId]);
+
   return (
     <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10 '>
-      <h1 className='text-3xl font-medium'>Top Doctors to Book</h1>
+      <h1 className='text-3xl font-medium'>Related Doctors</h1>
         <p className='sm:w-1/3 text-center text-sm'>Simply browse through our extensive list of trusted doctors.</p>
 
 <div className='w-full grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-4 pt-5 gap-y-6 px-3 sm:px-0'>            {
-                doctors.slice(0,10).map((item,index)=>(
+                relDoc.slice(0,5).map((item,index)=>(
                     <div
-                    onClick={()=>{navigate(`/appointment/${item._id}`); scrollTo(0,0)}}                         key={index} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10x] transition-all duration-500'>
+                          onClick={()=>{navigate(`/appointment/${item._id}`); scrollTo(0,0)}}
+                         key={index} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10x] transition-all duration-500'>
                         <img className='bg-blue-50 ' 
                         src={item.image} alt=""/>
 
@@ -40,4 +50,4 @@ const TopDoctors = () => {
   )
 }
 
-export default TopDoctors
+export default RelatedDoctors
