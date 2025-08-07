6b6fb95f8183dc5498bc9efdcbc7cc504ce20789
meh5REeen <mehreens826@gmail.com>
2025-08-08 00:12:09 +0500

    admin auth middleware added

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -3,10 +3,11 @@ import express from 'express';
 import { addDoc, loginAdmin } from '../controllers/adminController.js';
 
 import upload from '../middlewares/multer.js';
+import { authAdmin } from '../middlewares/authadmin.js';
 
 const adminRouter = express.Router();
 
-adminRouter.post('/add-doctor',upload.single('image'),addDoc)
+adminRouter.post('/add-doctor',authAdmin,upload.single('image'),addDoc)
 adminRouter.post('/login',loginAdmin)
 
 export default adminRouter;
