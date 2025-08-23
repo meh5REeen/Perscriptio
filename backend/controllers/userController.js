0bc2bcef4ae211daa4de2fc79eaa7ac9524dacbe
meh5REeen <mehreens826@gmail.com>
2025-08-24 00:03:49 +0500

    user appointment showing done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -194,7 +194,16 @@ const bookAppointment = async (req,res) => {
 
 
 // API to get user appointments
+const listAppointment = async (req,res) => {
+    try{
+        const {userId} = req
+        const appointments = await appointmentModel.find({userId});
+        res.json({success:true,appointments})
+    }catch(error){
+        console.log(error)
+        res.json({success:false,message:error.message})
+    }
+}
 
 
-
-export {registerUser,loginUser,getProfile,updateProfile,bookAppointment}
+export {registerUser,listAppointment,loginUser,getProfile,updateProfile,bookAppointment}
